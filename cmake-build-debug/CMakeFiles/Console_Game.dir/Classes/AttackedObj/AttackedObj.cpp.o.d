ff9cc5a12fce92e56b22e33c21f5c4ba023731c7
AzMax22 <az.maks.work@mail.ru>
2024-10-25 12:01:34 +0300

    Remove stage cmake-build-debug/ gir

--- cmake-build-debug/CMakeFiles/Console_Game.dir/Classes/AttackedObj/AttackedObj.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Console_Game.dir/Classes/AttackedObj/AttackedObj.cpp.o: \
- /home/maxim/Рабочий\ стол/Вуз/3\ сем/OOP/ConsoleGame/Classes/AttackedObj/AttackedObj.cpp \
- /usr/include/stdc-predef.h \
- /home/maxim/Рабочий\ стол/Вуз/3\ сем/OOP/ConsoleGame/Classes/AttackedObj/AttackedObj.h
